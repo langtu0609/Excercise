69a9842e5405653acfb31147b4dc66eaea39d4b9
Phan Tan Phat <ptp@globeteam.com>
2020-06-13 00:00:03 +0700

    Update Test Project

--- CPlusPlusProject/CPlusPlusProject/Ex1.cpp
+++ CPlusPlusProject/CPlusPlusProject/Ex1.cpp
@@ -1,10 +1,15 @@
 int CalculateSum(int number)
 {
-	int sum = 0;
+	// Method 1
+	/*int sum = 0;
+	
 	for (int i = 1; i <= number; ++i)
 	{
 		sum = sum + i;
-	}
+	}*/
+
+	// Method 2
+	int sum = number * (1 + number) / 2;
 
 	return sum;
 }
